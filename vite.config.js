c78ac1370c336f2603128c969db78b9077f8d040
Nicolas Richard <nrichard@he2b.be>
2025-07-04 11:43:16 +0200

    vite.config.js: use defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-export default {
+import { defineConfig } from "vite"
+
+export default defineConfig({
     server: {
         allowedHosts: ["nri.intranet.he2b.be"] // defined locally in my /etc/hosts file
     },
-    base: '/generateur-attestations'
-}
+    base: '/generateur-attestations'  
+})
